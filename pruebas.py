55c5d0a02398a9082da721167610c14c5a9d0199
womber21 <faba199921@gmail.com>
2023-09-17 19:11:08 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,18 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.constants as cons
+import math
 
-# Generate a range of frequencies
-start_frequency = 1e6
-end_frequency = 1e10
-step_frequency = 1e9  # Adjust this step size as needed
-frequencies = np.arange(start_frequency, end_frequency, step_frequency)
 
+start_epsilon = 0.01
+end_epsilon = 10
+step_epsilon = 0.05
+epsilon = np.arange(start_epsilon, end_epsilon,step_epsilon)
+square= np.arange(start_epsilon, end_epsilon,step_epsilon)
 # Corresponding electric permittivity values (example)
-permittivity_values = np.array([4.5, 4.2, 4.0, 3.8, 3.5, 3.2, 2.9, 2.6, 2.3, 2.0])
+n=math.sqrt(cons.epsilon_0*cons.mu_0)
+square=np.multiply(epsilon,n)
+
 
 # Create the plot
 plt.figure(figsize=(8, 6))
-plt.plot(frequencies, permittivity_values, marker='o', linestyle='-')
+plt.plot(epsilon, square, marker='o', linestyle='-')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Electric Permittivity (ε)')
 plt.title('Electric Permittivity vs. Frequency')
